8bfae9a0b313dac1823400edc5c94460f8f19230
Paulo J. Cagol <paulojosecagol@gmail.com>
2021-02-23 23:55:40 -0300

    3. Testando casos de erro

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,52 +1,105 @@
+import 'dart:convert';
+
 import 'package:faker/faker.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:fordev/data/http/http.dart';
 import 'package:http/http.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
 class ClientSpy extends Mock implements Client {}
 
-class HttpAdapter {
+class HttpAdapter implements HttpClient {
   final Client client;
-  final hedears = {
-    'content-type': 'application/json',
-    'accept': 'application/json'
-  };
 
   HttpAdapter(this.client);
 
-  Future<void> request({
-    @required String url,
-    @required String method,
-  }) async {
-    await client.post(url, headers: hedears);
+  Future<Map> request(
+      {@required String url, @required String method, Map body}) async {
+    final headers = {
+      'content-type': 'application/json',
+      'accept': 'application/json'
+    };
+    final jsonBody = body != null ? jsonEncode(body) : null;
+    final response = await client.post(url, headers: headers, body: jsonBody);
+    if (response.statusCode == 200) {
+      return response.body.isEmpty ? null : jsonDecode(response.body);
+    } else {
+      return null;
+    }
   }
 }
 
 void main() {
-  group(
-    'post',
-    () {
-      test(
-        'Should call post with correct values',
-        () async {
-          final client = ClientSpy();
-          final sut = HttpAdapter(client);
-          final url = faker.internet.httpsUrl();
+  HttpAdapter sut;
+  ClientSpy client;
+  String url;
 
-          await sut.request(url: url, method: 'post');
+  setUp(() {
+    client = ClientSpy();
+    sut = HttpAdapter(client);
+    url = faker.internet.httpUrl();
+  });
 
-          verify(
-            client.post(
-              url,
-              headers: {
-                'content-type': 'application/json',
-                'accept': 'application/json'
-              },
-            ),
-          );
-        },
-      );
-    },
-  );
+  group('post', () {
+    PostExpectation mockRequest() => when(
+        client.post(any, body: anyNamed('body'), headers: anyNamed('headers')));
+
+    void mockResponse(int statusCode,
+        {String body = '{"any_key":"any_value"}'}) {
+      mockRequest().thenAnswer((_) async => Response(body, statusCode));
+    }
+
+    setUp(() {
+      mockResponse(200);
+    });
+
+    test('Should call post with correct values', () async {
+      await sut
+          .request(url: url, method: 'post', body: {'any_key': 'any_value'});
+
+      verify(client.post(url,
+          headers: {
+            'content-type': 'application/json',
+            'accept': 'application/json'
+          },
+          body: '{"any_key":"any_value"}'));
+    });
+
+    test('Should call post without body', () async {
+      await sut.request(url: url, method: 'post');
+
+      verify(client.post(any, headers: anyNamed('headers')));
+    });
+
+    test('Should return data if post returns 200', () async {
+      final response = await sut.request(url: url, method: 'post');
+
+      expect(response, {'any_key': 'any_value'});
+    });
+
+    test('Should return null if post returns 200 with no data', () async {
+      mockResponse(200, body: '');
+
+      final response = await sut.request(url: url, method: 'post');
+
+      expect(response, null);
+    });
+
+    test('Should return null if post returns 204', () async {
+      mockResponse(204, body: '');
+
+      final response = await sut.request(url: url, method: 'post');
+
+      expect(response, null);
+    });
+
+    test('Should return null if post returns 204 with data', () async {
+      mockResponse(204);
+
+      final response = await sut.request(url: url, method: 'post');
+
+      expect(response, null);
+    });
+  });
 }
