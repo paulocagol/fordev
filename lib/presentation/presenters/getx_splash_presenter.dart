b8dc9ae0040cb2e749274752058b2edfe105a536
Paulo J. Cagol <paulojosecagol@gmail.com>
2021-03-04 23:45:53 -0300

    5. Criando o Presenter para o Splash Page com GetX

---
+++ lib/presentation/presenters/getx_splash_presenter.dart
@@ -0,0 +1,23 @@
+import 'package:fordev/domain/usecases/usecases.dart';
+import 'package:fordev/ui/pages/splash/splash_presenter.dart';
+import 'package:get/get.dart';
+import 'package:meta/meta.dart';
+
+class GetxSplashPresenter implements SplashPresenter {
+  final LoadCurrentAccount loadCurrentAccount;
+
+  var _navigateTo = RxString();
+
+  GetxSplashPresenter({@required this.loadCurrentAccount});
+
+  Stream<String> get navigateToStream => _navigateTo.stream;
+
+  Future<void> checkAccount() async {
+    try {
+      final account = await loadCurrentAccount.load();
+      _navigateTo.value = account == null ? '/login' : '/surveys';
+    } catch (e) {
+      _navigateTo.value = '/login';
+    }
+  }
+}
---
+++ test/presentation/presenters/getx_plash_presenter_test.dart
@@ -0,0 +1,55 @@
+import 'package:faker/faker.dart';
+import 'package:fordev/domain/entities/account_entity.dart';
+import 'package:fordev/domain/usecases/load_current_account.dart';
+import 'package:fordev/presentation/presenters/getx_splash_presenter.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+
+class LoadCurrentAccountSpy extends Mock implements LoadCurrentAccount {}
+
+void main() {
+  LoadCurrentAccountSpy loadCurrentAccount;
+  GetxSplashPresenter sut;
+  PostExpectation mockLoadCurrentAccountCall() => when(loadCurrentAccount.load());
+
+  void mockLoadCurrentAccount({AccountEntity account}) {
+    mockLoadCurrentAccountCall().thenAnswer((_) async => account);
+  }
+
+  void mockLoadCurrentAccountError() {
+    mockLoadCurrentAccountCall().thenThrow(Exception);
+  }
+
+  setUp(() {
+    loadCurrentAccount = LoadCurrentAccountSpy();
+    sut = GetxSplashPresenter(loadCurrentAccount: loadCurrentAccount);
+    mockLoadCurrentAccount(account: AccountEntity(faker.guid.guid()));
+  });
+
+  test('Should call LoadCurrentAccount', () async {
+    await sut.checkAccount();
+    verify(loadCurrentAccount.load()).called(1);
+  });
+
+  test('Should go to surveys on success', () async {
+    sut.navigateToStream.listen(expectAsync1((page) => expect(page, '/surveys')));
+
+    await sut.checkAccount();
+  });
+
+  test('Should go to login page on null result', () async {
+    mockLoadCurrentAccount(account: null);
+
+    sut.navigateToStream.listen(expectAsync1((page) => expect(page, '/login')));
+
+    await sut.checkAccount();
+  });
+
+  test('Should go to login page on error', () async {
+    mockLoadCurrentAccountError();
+
+    sut.navigateToStream.listen(expectAsync1((page) => expect(page, '/login')));
+
+    await sut.checkAccount();
+  });
+}
