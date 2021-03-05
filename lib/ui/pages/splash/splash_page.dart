9bc709e2887625690a17baba7ec327e2a44de3f6
Paulo J. Cagol <paulojosecagol@gmail.com>
2021-03-04 23:11:58 -0300

    4. Criando a tela de Splash

---
+++ lib/ui/pages/splash/splash_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:fordev/ui/pages/splash/splash.dart';
+import 'package:get/get.dart';
+
+class SplashPage extends StatelessWidget {
+  final SplashPresenter presenter;
+
+  const SplashPage({@required this.presenter});
+
+  @override
+  Widget build(BuildContext context) {
+    presenter.loadCurrentAccount();
+    return Scaffold(
+      appBar: AppBar(title: Text('fordev')),
+      body: Builder(
+        builder: (context) {
+          presenter.navigateToStream.listen((page) {
+            if (page?.isNotEmpty == true) {
+              Get.offAllNamed(page);
+            }
+          });
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ test/ui/pages/splash_Page_test.dart
@@ -0,0 +1,55 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:fordev/ui/pages/splash/splash.dart';
+import 'package:get/get.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+import 'package:mockito/mockito.dart';
+
+class SplashPresenterSpy extends Mock implements SplashPresenter {}
+
+void main() {
+  SplashPresenterSpy presenter;
+  StreamController<String> navigateToController;
+
+  Future<void> loadPage(WidgetTester tester) async {
+    presenter = SplashPresenterSpy();
+    navigateToController = StreamController<String>();
+    when(presenter.navigateToStream).thenAnswer((_) => navigateToController.stream);
+
+    await tester.pumpWidget(
+      GetMaterialApp(
+        initialRoute: '/',
+        getPages: [
+          GetPage(name: '/', page: () => SplashPage(presenter: presenter)),
+          GetPage(name: '/any_route', page: () => Scaffold(body: Text('fake page'))),
+        ],
+      ),
+    );
+  }
+
+  tearDown(() {
+    navigateToController.close();
+  });
+
+  testWidgets('Should present snippner on page load', (WidgetTester tester) async {
+    await loadPage(tester);
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+  });
+
+  testWidgets('Should call loadCurrentAccount on page load', (WidgetTester tester) async {
+    await loadPage(tester);
+    verify(presenter.loadCurrentAccount()).called(1);
+  });
+
+  testWidgets('Should load page', (WidgetTester tester) async {
+    await loadPage(tester);
+
+    navigateToController.add('/any_route');
+    await tester.pumpAndSettle();
+
+    expect(Get.currentRoute, '/any_route');
+    expect(find.text('fake page'), findsOneWidget);
+  });
+}
